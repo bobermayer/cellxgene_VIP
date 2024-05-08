5d1ac9ef941eeac9d588feea4529d27855e7824d
bobermayer <benedikt.obermayer@bih-charite.de>
2024-05-08 13:54:24 +0200

    update build_docker

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BUILD_NO=0
+git describe --tags | cut -d - -f 10UILD_NO=1
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
@@ -9,7 +9,7 @@ GIT_DESCRIBE=$(git describe --tags | cut -d - -f 1)
 GIT_TAG=${GIT_TAG-$GIT_DESCRIBE}
 DOCKER_VERSION=$(echo $GIT_TAG | sed -e 's/^v//')-$BUILD_NO
 GIT_DEPTH=$(($(git rev-list HEAD ^$(git describe --abbrev=0 --tags) --count) + 1))
-GIT_URL=https://github.com/bihealth/scelvis.git
+GIT_URL=https://github.com/bobermayer/cellxgene_VIP.git
 
 docker build . \
     --build-arg app_git_tag=$GIT_TAG \
